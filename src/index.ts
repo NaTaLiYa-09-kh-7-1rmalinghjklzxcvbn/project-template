4e9945add8f25a0ec04caddd2bc86cdc07fc160d
gitbrains <info@geekbrains.ru>
2023-01-20 22:48:30 +0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { renderSearchFormBlock } from './search-form.js'
+import { renderSearchStubBlock } from './search-results.js'
+import { renderUserBlock } from './user.js'
+import { renderToast } from './lib.js'
+
+const avatar = "/img/avatar.png"
+const userName = "Wade Warren"
+const objectNumber = 0
+window.addEventListener('DOMContentLoaded', () => {
+  renderUserBlock(userName, avatar, objectNumber)
+  renderSearchFormBlock()
+  renderSearchStubBlock()
+  renderToast(
+    { text: 'Это пример уведомления. Используйте его при необходимости', type: 'success' },
+    { name: 'Понял', handler: () => { console.log('Уведомление закрыто') } }
+  )
+})
